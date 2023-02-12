8b460af190a96418ae05017602ba2bc8748652de
CK01 <trancanh1201@gmail.com>
2023-02-12 16:20:09 +0700

    add neccessary method and small edits

--- lib/pages/dashboard/dashboard_view_model.dart
+++ lib/pages/dashboard/dashboard_view_model.dart
@@ -54,18 +54,28 @@ class DashboardViewModel extends _$DashboardViewModel {
     // await Future.delayed(const Duration(seconds: 2));
 
     // setImageUrl(Constants.image1);
+    setImageUrl(
+        'https://oaidalleapiprodscus.blob.core.windows.net/private/org-hrmTDhMF7EYMENxPVjKbrjIy/user-ncs1YMXaZbAjskqynnW7Ps2P/img-lY2LeM8vN33Y8G1pnnktobxr.png?st=2023-02-12T07%3A17%3A36Z&se=2023-02-12T09%3A17%3A36Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-02-11T21%3A38%3A08Z&ske=2023-02-12T21%3A38%3A08Z&sks=b&skv=2021-08-06&sig=MaoE5gGIkV7OjagdnDDtl4Hnz/tIJWu6q0Qx%2BDXHlgw%3D');
 
-    final imageUrl = await apiClient.createImage(
-      textDescription: state.inputText!,
-    );
+    // final imageUrl = await apiClient.createImage(
+    //   textDescription: state.inputText!,
+    // );
 
-    if (imageUrl.isNotEmpty) {
-      setImageUrl(imageUrl);
-      logger.i('Generate image successfully.');
-    } else {
-      logger.e('Generate image failed.');
-    }
+    // if (imageUrl.isNotEmpty) {
+    //   setImageUrl(imageUrl);
+    //   logger.i('Generate image successfully.');
+
+    //   if (!state.isSaveBtnActive) {
+    //     setsIsSaveBtnActive(true);
+    //   }
+    // } else {
+    //   logger.e('Generate image failed.');
+    // }
 
     setIsGeneratingImage(false);
+  }
+
+  Future<void> downloadImage() async {
+
   }
 }
